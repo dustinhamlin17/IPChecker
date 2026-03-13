ca55edf5a78900375b97a52ec1d86a19f0025d9e
Dustin <dustinhamlin17@gmail.com>
2026-03-13 11:02:38 -0400

    Implemented duplicate clean up

--- modifiedIPSearch.py
+++ modifiedIPSearch.py
@@ -27,12 +27,23 @@ df = pandas.read_csv(file_path, dtype="object")
 df2 = df
 content = df2.iloc[:, 1:7].to_string()
 
+
 print("Extracting IPs...")
 
 # This section is the regex string used to detect IP addresses that are passed to the "ipcheck" variable, it uses the findall method against the ipcheck vairable and stores it in the match vairable
 
 ip = re.compile(r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}")
 match = ip.findall(content)
+
+print("Cleaning up duplicates...")
+
+undupedmatch = []
+
+for ipaddresses in match:
+    if ipaddresses not in undupedmatch:
+        undupedmatch.append(ipaddresses)
+    else:
+        continue
 
 #This section determines if the IP address found is a public or private IP address, we only want public IPs
 
@@ -41,7 +52,7 @@ public = []
 print("Checking Database...")
 
 try:
-    for validaddresses in match:
+    for validaddresses in undupedmatch:
         if ipaddress.IPv4Address(validaddresses).is_global == True:
             public.append(validaddresses)
         else:
@@ -76,10 +87,11 @@ else:
 
 # Current issues: if pulled IP is private, it does not get stored in address and therefore causes NameError for some reason
 
-# Need to proper JSON parsing so that only the IP address, confidence score, domain, total reports and country code are returned 
+# Need to discard duplicate IPs
+
+# Need to do proper JSON parsing so that only the IP address, confidence score, domain, total reports and country code are returned 
 
 # Would like to have it prompt the user to enter their API key in which it would store it temporarily until the program closes (or just have the user add a .env file in the folder with their API key)
-
 
 
 
